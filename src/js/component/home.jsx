2e93d6d75a5a041d484b3ab656e1c7363f0470e5
Brayan2JZ <b.sredzinski92@gmail.com>
2024-09-05 00:56:42 +0000

    Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,7 +13,7 @@ export function Home() {
 			<div onClick={() => setSelectedColor("red")} className={"light red" + (selectedColor === "red" ? " glow" : "")}></div>
 			<div onClick={() => setSelectedColor("yellow")} className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}></div>
 			<div onClick={() => setSelectedColor("green")} className={"light green" + (selectedColor === "green" ? " glow" : "")}></div>
-		</div>
+			</div>
 	);
 };
 
